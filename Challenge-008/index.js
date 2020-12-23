b6e0b904994f52aa920582877cbae69aa74de266
Xandowski <xandy-moraes@hotmail.com>
2020-12-23 20:08:33 -0300

    Challenge-008 completed

--- Challenge-008/index.js
+++ Challenge-008/index.js
@@ -1,5 +1,21 @@
-const imgContainer = document.querySelector('.img-container')
+const imgContainer = document.querySelectorAll('.img-container')
+const fromTop = document.querySelector('.from-top')
+const fromBottom = document.querySelector('.from-bottom')
+const photo = document.querySelector('.photo')
+const backBtn = document.querySelector('.btn')
 
-imgContainer.addEventListener('click', () => {
-  
+for(let i = 0; i < 4; i++) {
+  imgContainer[i].addEventListener('click', () => {
+    fromTop.classList.toggle('-from-top-active')
+    fromBottom.classList.toggle('-from-bottom-active')
+    photo.classList.toggle('-photo-active')
+  })
+}
+
+backBtn.addEventListener('click', () => {
+  fromTop.classList.remove('-from-top-active')
+  fromBottom.classList.remove('-from-bottom-active')
+  photo.classList.remove('-photo-active')
 })
+
+console.log(imgContainer)
